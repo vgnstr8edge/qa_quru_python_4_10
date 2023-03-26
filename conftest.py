0128f1bae76f64a2400479a609c81f22e855b5f2
Dmitrii Nasedkin <2_tone@mail.ru>
2023-03-26 16:18:21 +0300

    изменил таймаут в master

--- conftest.py
+++ conftest.py
@@ -7,9 +7,8 @@ def browser_settings():
     browser.config.hold_browser_open = True
     browser.open('https://demoqa.com/automation-practice-form')
     browser.driver.maximize_window()
-    browser.config.timeout = 6.0
-    #browser.config.window_height = 1920
-    #browser.config.window_width = 1080
+    browser.config.timeout = 10.0
+
 
 
 
